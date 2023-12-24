6c0c5489ea9be08f72afa38453b175833188da79
Ilya udalov <ilya_udalov_04@mail.ru>
2023-12-24 19:05:23 +0300

    cleanup

--- utils.sql
+++ utils.sql
@@ -5,16 +5,19 @@ CREATE ROLE trainer_role;
 
 -- Присвоение ролей пользователям
 CREATE USER admin PASSWORD 'admin_password';
-CREATE USER user PASSWORD 'user_password';
+CREATE USER client PASSWORD 'user_password';
 CREATE USER trainer PASSWORD 'trainer_password';
 
 GRANT admin_role TO admin;
-GRANT user_role TO user;
-GRANT trainer_role TO rainer;
+GRANT user_role TO client;
+GRANT trainer_role TO trainer;
 
 -- Грант привилегий
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO admin_role;
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES athletes, nutrition, workouts, measurements, diet_profiles, training_programs, training_sessions, endurance_tests TO user_role, trainer_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE diet_profiles TO trainer_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE diet_profiles TO admin_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE training_programs TO trainer_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE training_programs TO admin_role;
+
 
 -- Определение политик доступа
 
@@ -27,12 +30,15 @@ CREATE POLICY diet_profiles_admin_policy ON diet_profiles
 CREATE POLICY diet_profiles_trainer_policy ON diet_profiles
   FOR ALL
   TO trainer_role
-  USING (true);
+  USING (true)
+  WITH CHECK (true);
 
 CREATE POLICY diet_profiles_user_policy ON diet_profiles
   FOR SELECT
   TO user_role
-  USING (true);
+  USING (true)
+  WITH CHECK (true);
+
 
 -- Для таблицы "training_programs"
 CREATE POLICY training_programs_admin_policy ON training_programs
@@ -44,6 +50,8 @@ CREATE POLICY training_programs_trainer_policy ON training_programs
   FOR ALL
   TO trainer_role
   USING (true);
+  WITH CHECK (true);
+
 
 CREATE POLICY training_programs_user_policy ON training_programs
   FOR SELECT
